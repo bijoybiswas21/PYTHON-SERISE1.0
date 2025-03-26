b0a464bae9b92953a58bad5cccbaf5566ed3b306
Bijoy Biswas <156492281+bijoybiswas21@users.noreply.github.com>
2025-03-25 16:33:25 -1100

    Create pibonacci.py

---
+++ Python1/pibonacci.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+n_terms = int(input("How many terms of the Fibonacci series to print? "))
+print(a, b, end=' ')
+for _ in range(n_terms - 2):
+    c = a + b
+    print(c, end=' ')
+    a = b
+    b = c
+print()
